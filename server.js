4c84ca0532a9d4bcc913614557d4bd69779531ba
ASHWIN CLARENCE <meashwinclarence@gmail.com>
2024-04-05 19:00:42 +0530

    ejs files created

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express=require('express')
+const session=require('express-session')
+const path=require('path')
+const bodyParser=require('body-parser')
+const nocache=require('nocache')
+const {v4:uuidv4}=require('uuid')
 
-
-const port=process.env.PORT||3000;
 const app=express()
 
+const port=process.env.PORT||3000;
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(session({
+    secret:uuidv4(),
+    resave:true,
+    saveUninitialized:true
+}))
 
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+    res.render('login',{title:'Login Page'})
+})
 
 app.listen(port,(err)=>{
     if(err){
